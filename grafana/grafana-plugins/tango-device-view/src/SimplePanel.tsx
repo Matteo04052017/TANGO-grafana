0f93164edd8dac1d88fa87016f008c22bb278c6a
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2020-05-19 15:35:25 +0000

    spectrum (not ordered)

--- grafana/grafana-plugins/tango-device-view/src/SimplePanel.tsx
+++ grafana/grafana-plugins/tango-device-view/src/SimplePanel.tsx
@@ -28,8 +28,8 @@ createTheme('solarized', {
 const customStyles = {
   headCells: {
     style: {
-      fontSize: '22px',
-      fontWeight: 1000,
+      fontSize: '18px',
+      fontWeight: 800,
     },
   },
 };
@@ -45,6 +45,7 @@ const columns = [
     name: 'Value',
     selector: 'value',
     sortable: true,
+    wrap: true,
     conditionalCellStyles: [
       {
         when: (row: any) => row.value === 'ON',
@@ -73,10 +74,23 @@ const columns = [
 interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const device_attributes = data.series.map(series => series.fields.find(field => field.type === 'number'));
+  const device_attributes = data.series.map(series =>
+    series.fields.find(field => field.type === 'number' && field.labels?.dim_x === '1' && field.labels?.dim_y === '0')
+  );
 
+  const device_attributes_spectrum = data.series.map(series =>
+    series.fields.find(field => field.type === 'number' && field.labels?.dim_x !== '1' && field.labels?.dim_y === '0')
+  );
+
+  // const device_attributes_image = data.series.map(series => series.fields.find(field => field.type === 'number' && field.labels?.dim_x !== "1" && field.labels?.dim_y !== "0"));
+  //console.log(device_attributes_spectrum);
   const attributes = [];
+
   for (let i = 0; i < device_attributes.length; i++) {
+    if (!device_attributes[i]?.labels?.name) {
+      continue;
+    }
+
     let j = device_attributes[i]?.values.length;
     if (!j) {
       j = 1;
@@ -86,11 +100,61 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
       this_value = device_attributes[i]?.labels?.str_value;
     }
 
+    //console.log('adding ' + device_attributes[i]?.labels?.name + ' value=' + this_value + ']');
     attributes.push({
       name: device_attributes[i]?.labels?.name,
       value: this_value,
     });
   }
+
+  let last_name = 'init';
+  let last_value = '[';
+  for (let i = 0; i < device_attributes_spectrum.length; i++) {
+    let this_name = device_attributes_spectrum[i]?.labels?.name;
+    // console.log('processing ' + this_name);
+    if (!this_name) {
+      continue;
+    }
+    if (attributes.find(attr => attr.name === this_name)) {
+      continue;
+    }
+
+    let j = device_attributes_spectrum[i]?.values.length;
+    if (!j) {
+      j = 1;
+    }
+    let this_value = device_attributes_spectrum[i]?.values?.get(j - 1);
+    if (
+      device_attributes_spectrum[i]?.labels?.type === 'string' ||
+      device_attributes_spectrum[i]?.labels?.type === 'state'
+    ) {
+      this_value = device_attributes_spectrum[i]?.labels?.str_value;
+    }
+
+    // console.log('this_name=' + this_name);
+    // console.log('last_name=' + last_name);
+
+    if (last_name === 'init') {
+      last_name = this_name;
+    }
+    if (this_name === last_name) {
+      if (last_value === '[') {
+        last_value = '[' + this_value;
+      } else {
+        last_value = last_value + ',' + this_value;
+      }
+    }
+    if (this_name !== last_name) {
+      attributes.push({
+        name: last_name,
+        value: last_value + ']',
+      });
+      last_name = this_name;
+      last_value = '[' + this_value;
+    }
+  }
+
+  console.log('attributes length ' + attributes.length);
 
   return (
     <DataTable
@@ -99,7 +163,6 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
       data={attributes}
       theme="solarized"
       customStyles={customStyles}
-      pagination
     />
   );
 };
