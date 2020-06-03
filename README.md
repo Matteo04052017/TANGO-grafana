4228ab2f2ed8ab22b5696e259e180c0ddc2d7865
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2020-06-03 15:07:42 +0000

    with archiver

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 An investigations on the modifiability of Prometheus and grafana in a TANGO-controls context
 
 # run on Kubernetes 
-kubectl create namespace tango-grafana
-kubectl create -f yaml/
+helm install tango-grafana0 helm-chart/
--- exporter/get_metrics.sh
+++ exporter/get_metrics.sh
@@ -1,1 +1,1 @@
-curl $(kubectl get svc -n integration -o jsonpath='{.items[?(@.metadata.name=="tango-exporter-service")].spec.clusterIP}')/metrics
+curl $(kubectl get svc -n tango-grafana -o jsonpath='{.items[?(@.metadata.name=="tango-exporter-service-0")].spec.clusterIP}')/metrics
